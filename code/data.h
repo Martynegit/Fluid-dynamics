456f1a07cec731e18f08e2c2590939d1b00827c9
Martina <martina.rovito2003@gmail.com>
2025-03-18 21:10:30 +0100

    Update data.h

--- code/data.h
+++ code/data.h
@@ -1,13 +1,18 @@
 #ifndef DATA_H
 #define DATA_H
 
+#include <string>
+
 class data{
 
   private:
+    std::string nFile;
   
   public:
     data();
-
+    data(std::string);
+    write(std::string, float);
+    read(std::string, float);
 
 };
 
